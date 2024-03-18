4bce5acaace048ae6137ab05041c5440a127d732
Rahul <ra1290hul@gmail.com>
2024-03-18 20:33:05 +0530

    initial commit

---
+++ Day37 Todo List/script.js
@@ -0,0 +1,20 @@
+function addTask() {
+  var inputField = document.getElementById("taskInput");
+  var taskList = document.getElementById("taskList");
+
+  if (inputField.value !== "") {
+    var li = document.createElement("li");
+    li.textContent = inputField.value;
+
+    var deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.classList.add("del");
+
+    li.appendChild(deleteButton);
+    taskList.appendChild(li);
+
+    inputField.value = "";
+  } else {
+    alert("Please enter a task!");
+  }
+}
