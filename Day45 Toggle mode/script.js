2d9ec47bce34462a2416b86b154988a4b5b2e817
Rahul <ra1290hul@gmail.com>
2024-03-29 20:27:15 +0530

    initial commit

--- Day45 Toggle mode/script.js
+++ Day45 Toggle mode/script.js
@@ -1,15 +1,5 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const modeToggleBtn = document.getElementById("mode-toggle");
-  let darkMode = false;
-
-  modeToggleBtn.addEventListener("click", function () {
-    darkMode = !darkMode;
-    if (darkMode) {
-      document.body.classList.add("dark-mode");
-      document.body.classList.remove("light-mode");
-    } else {
-      document.body.classList.add("light-mode");
-      document.body.classList.remove("dark-mode");
-    }
-  });
-});
+function toggleMode() {
+  const body = document.body;
+  body.classList.toggle("light-mode");
+  body.classList.toggle("dark-mode");
+}
